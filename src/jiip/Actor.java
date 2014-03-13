2987e2ba90e85008f2581dc14cb1e4d2190b3d55
Simone Miraglia <simone.miraglia@gmail.com>
2014-03-13 17:02:56 +0100

    First commit.

---
+++ src/jiip/Actor.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package jiip;
+
+/**
+ * 
+ * @author Simone Miraglia
+ *
+ */
+public class Actor {
+
+	/**
+	 * @param args argomenti
+	 */
+	public static void main(String[] args) {
+		System.out.println("Hello");
+
+	}
+
+}
